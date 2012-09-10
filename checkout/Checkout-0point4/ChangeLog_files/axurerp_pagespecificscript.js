d5bdc753faa80a6afe3dba2c88c8d3509522d36c
Jack Reis <jack@Jacks-MacBook-Pro.local>
2012-09-10 10:51:58 -0700

    add checkout files for versions 0.4 and 0.5, also update email-final, checkout-notes and checkout-prototypes

---
+++ checkout/Checkout-0point4/ChangeLog_files/axurerp_pagespecificscript.js
@@ -0,0 +1,31 @@
+﻿for(var i = 0; i < 44; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+gv_vAlignTable['u20'] = 'top';gv_vAlignTable['u21'] = 'top';gv_vAlignTable['u25'] = 'top';gv_vAlignTable['u26'] = 'top';gv_vAlignTable['u27'] = 'top';gv_vAlignTable['u28'] = 'top';gv_vAlignTable['u29'] = 'top';gv_vAlignTable['u30'] = 'top';gv_vAlignTable['u31'] = 'top';gv_vAlignTable['u32'] = 'top';gv_vAlignTable['u33'] = 'top';gv_vAlignTable['u34'] = 'top';gv_vAlignTable['u35'] = 'top';gv_vAlignTable['u36'] = 'top';gv_vAlignTable['u37'] = 'top';document.getElementById('u38_img').tabIndex = 0;
+
+u38.style.cursor = 'pointer';
+$axure.eventManager.click('u38', function(e) {
+
+if (true) {
+
+SetGlobalVariableValue('Shipping', 'Free');
+
+	NewTab($axure.globalVariableProvider.getLinkUrl('Home.html'), "");
+
+}
+});
+gv_vAlignTable['u39'] = 'center';gv_vAlignTable['u1'] = 'top';gv_vAlignTable['u3'] = 'top';gv_vAlignTable['u4'] = 'top';gv_vAlignTable['u5'] = 'top';gv_vAlignTable['u6'] = 'top';gv_vAlignTable['u7'] = 'top';gv_vAlignTable['u8'] = 'top';gv_vAlignTable['u9'] = 'top';gv_vAlignTable['u42'] = 'top';gv_vAlignTable['u40'] = 'top';gv_vAlignTable['u41'] = 'top';gv_vAlignTable['u43'] = 'top';gv_vAlignTable['u10'] = 'top';gv_vAlignTable['u11'] = 'top';gv_vAlignTable['u12'] = 'top';gv_vAlignTable['u13'] = 'top';document.getElementById('u14_img').tabIndex = 0;
+
+u14.style.cursor = 'pointer';
+$axure.eventManager.click('u14', function(e) {
+
+if (true) {
+
+	NewTab($axure.globalVariableProvider.getLinkUrl('Home.html'), "");
+
+}
+});
+gv_vAlignTable['u15'] = 'center';gv_vAlignTable['u17'] = 'top';gv_vAlignTable['u18'] = 'top';gv_vAlignTable['u19'] = 'top';
---
+++ checkout/Checkout-0point5/Home_files/axurerp_pagespecificscript.js
@@ -0,0 +1,610 @@
+﻿for(var i = 0; i < 142; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+if ((GetGlobalVariableValue('ItemsInCart')) != ('0')) {
+
+SetWidgetRichText('u128', '<p style="text-align:center;"><span style="font-family:Arial;font-size:13px;font-weight:normal;font-style:normal;text-decoration:none;color:#333333;">CHECKOUT (</span><span style="font-family:Arial;font-size:13px;font-weight:bold;font-style:normal;text-decoration:none;color:#333333;">' + (GetGlobalVariableValue('ItemsInCart')) + '</span><span style="font-family:Arial;font-size:13px;font-weight:normal;font-style:normal;text-decoration:none;color:#333333;">)</span></p>');
+
+}
+
+if ((GetWidgetText('u127')) == ('CHECKOUT ()')) {
+
+SetWidgetRichText('u128', '<p style="text-align:center;"><span style="font-family:Arial;font-size:13px;font-weight:normal;font-style:normal;text-decoration:none;color:#333333;">CHECKOUT (</span><span style="font-family:Arial;font-size:13px;font-weight:bold;font-style:normal;text-decoration:none;color:#333333;">0</span><span style="font-family:Arial;font-size:13px;font-weight:normal;font-style:normal;text-decoration:none;color:#333333;">)</span></p>');
+
+}
+
+if ((GetGlobalVariableValue('CanWeShipVar')) == ('Yes')) {
+
+	SetPanelState('u133', 'pd1u133','none','',500,'none','',500);
+
+}
+
+});
+gv_vAlignTable['u81'] = 'center';gv_vAlignTable['u82'] = 'top';gv_vAlignTable['u83'] = 'top';u84.tabIndex = 0;
+
+u84.style.cursor = 'pointer';
+$axure.eventManager.click('u84', function(e) {
+
+if (true) {
+
+	SetPanelState('u75', 'pd0u75','fade','',500,'fade','',500);
+
+	SetPanelState('u64', 'pd0u64','none','',500,'none','',500);
+
+}
+});
+u85.tabIndex = 0;
+
+u85.style.cursor = 'pointer';
+$axure.eventManager.click('u85', function(e) {
+
+if (true) {
+
+	SetPanelState('u64', 'pd1u64','none','',500,'none','',500);
+
+	SetPanelState('u75', 'pd1u75','fade','',500,'fade','',500);
+
+}
+});
+gv_vAlignTable['u85'] = 'top';document.getElementById('u86_img').tabIndex = 0;
+HookHover('u86', false);
+HookClick('u86', false);
+
+u86.style.cursor = 'pointer';
+$axure.eventManager.click('u86', function(e) {
+
+if (((GetGlobalVariableValue('Hurry')) == ('Yes')) && ((GetWidgetText('u92')) == ('94025'))) {
+
+SetGlobalVariableValue('CanWeShipVar', 'Yes');
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('View_Cart.html');
+
+}
+
+if ((GetWidgetText('u92')) == ('94025')) {
+
+SetGlobalVariableValue('CanWeShipVar', 'Yes');
+
+	SetPanelState('u28', 'pd2u28','none','',500,'none','',500);
+
+}
+else
+if (true) {
+
+SetGlobalVariableValue('CanWeShipVar', 'No');
+
+	SetPanelState('u28', 'pd1u28','none','',500,'none','',500);
+
+}
+});
+gv_vAlignTable['u87'] = 'center';gv_vAlignTable['u89'] = 'center';gv_vAlignTable['u90'] = 'top';u91.tabIndex = 0;
+
+u91.style.cursor = 'pointer';
+$axure.eventManager.click('u91', function(e) {
+
+if (true) {
+
+	SetPanelVisibility('u28','hidden','none',500);
+
+}
+});
+gv_vAlignTable['u91'] = 'top';$('#u92').attr('axSubmit', 'u69');
+
+$axure.eventManager.focus('u92', function(e) {
+
+if (true) {
+
+SetWidgetFormText('u92', '');
+
+}
+});
+
+$axure.eventManager.blur('u92', function(e) {
+
+if ((GetWidgetText('u92')) == ('')) {
+
+SetWidgetFormText('u92', 'Enter Your ZIP Code…');
+
+}
+});
+u10.tabIndex = 0;
+
+u10.style.cursor = 'pointer';
+$axure.eventManager.click('u10', function(e) {
+
+if (true) {
+
+	SetPanelVisibility('u0','hidden','none',500);
+
+	SetPanelVisibility('u28','hidden','none',500);
+
+	SetPanelVisibility('u93','hidden','none',500);
+
+}
+});
+gv_vAlignTable['u10'] = 'top';gv_vAlignTable['u13'] = 'center';document.getElementById('u14_img').tabIndex = 0;
+
+u14.style.cursor = 'pointer';
+$axure.eventManager.click('u14', function(e) {
+
+if (true) {
+
+SetWidgetFormText('u18', '' + (GetNum(GetWidgetText('u18')) + 1) + '');
+
+}
+});
+gv_vAlignTable['u15'] = 'top';document.getElementById('u16_img').tabIndex = 0;
+
+u16.style.cursor = 'pointer';
+$axure.eventManager.click('u16', function(e) {
+
+if ((GetWidgetText('u18')) > Number('0')) {
+
+SetWidgetFormText('u18', '' + (GetWidgetText('u18') - 1) + '');
+
+}
+});
+gv_vAlignTable['u17'] = 'top';gv_vAlignTable['u110'] = 'center';gv_vAlignTable['u112'] = 'center';gv_vAlignTable['u114'] = 'center';gv_vAlignTable['u20'] = 'top';gv_vAlignTable['u21'] = 'top';document.getElementById('u22_img').tabIndex = 0;
+HookHover('u22', false);
+HookClick('u22', false);
+
+u22.style.cursor = 'pointer';
+$axure.eventManager.click('u22', function(e) {
+
+if ((GetGlobalVariableValue('Hurry')) != ('Yes')) {
+
+SetGlobalVariableValue('ItemsInCart', '' + (GetNum(GetGlobalVariableValue('ItemsInCart')) + GetNum(GetWidgetText('u18'))) + '');
+
+    self.location.href="resources/reload.html#" + encodeURI($axure.globalVariableProvider.getLinkUrl($axure.pageData.url));
+
+}
+
+if ((GetGlobalVariableValue('Hurry')) == ('Yes')) {
+
+SetGlobalVariableValue('ItemsInCart', '' + (GetNum(GetGlobalVariableValue('ItemsInCart')) + GetNum(GetWidgetText('u18'))) + '');
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('View_Cart.html');
+
+}
+});
+gv_vAlignTable['u23'] = 'center';gv_vAlignTable['u25'] = 'center';gv_vAlignTable['u27'] = 'center';gv_vAlignTable['u101'] = 'center';gv_vAlignTable['u102'] = 'top';u103.tabIndex = 0;
+
+u103.style.cursor = 'pointer';
+$axure.eventManager.click('u103', function(e) {
+
+if (true) {
+
+	SetPanelVisibility('u93','hidden','none',500);
+
+}
+});
+gv_vAlignTable['u103'] = 'top';gv_vAlignTable['u104'] = 'top';gv_vAlignTable['u105'] = 'top';document.getElementById('u106_img').tabIndex = 0;
+HookHover('u106', false);
+HookClick('u106', false);
+
+u106.style.cursor = 'pointer';
+$axure.eventManager.click('u106', function(e) {
+
+if (true) {
+
+	SetPanelVisibility('u93','hidden','none',500);
+
+}
+});
+gv_vAlignTable['u107'] = 'center';document.getElementById('u127_img').tabIndex = 0;
+HookHover('u127', false);
+HookClick('u127', false);
+
+u127.style.cursor = 'pointer';
+$axure.eventManager.click('u127', function(e) {
+
+if (((GetGlobalVariableValue('HasItem')) == ('Yes')) || ((GetGlobalVariableValue('ItemsInCart')) > Number('0'))) {
+
+SetGlobalVariableValue('ZIPVar', '94025');
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('View_Cart.html');
+
+}
+
+if (((GetGlobalVariableValue('HasItem')) != ('Yes')) && ((GetGlobalVariableValue('ItemsInCart')) < Number('1'))) {
+
+	SetPanelVisibility('u93','','none',500);
+
+	BringToFront("u93");
+
+}
+});
+gv_vAlignTable['u128'] = 'center';document.getElementById('u129_img').tabIndex = 0;
+HookHover('u129', false);
+HookClick('u129', false);
+
+u129.style.cursor = 'pointer';
+$axure.eventManager.click('u129', function(e) {
+
+if (true) {
+
+SetGlobalVariableValue('ZIPVar', 'Enter Your ZIP Code...');
+
+	SetPanelVisibility('u28','','none',500);
+
+	BringToFront("u28");
+
+}
+
+if ((GetGlobalVariableValue('CanWeShipVar')) == ('Yes')) {
+
+	SetPanelVisibility('u0','','none',500);
+
+	BringToFront("u0");
+
+}
+});
+gv_vAlignTable['u30'] = 'center';gv_vAlignTable['u32'] = 'center';gv_vAlignTable['u34'] = 'center';gv_vAlignTable['u36'] = 'center';gv_vAlignTable['u37'] = 'top';u38.tabIndex = 0;
+
+u38.style.cursor = 'pointer';
+$axure.eventManager.click('u38', function(e) {
+
+if (true) {
+
+	SetPanelVisibility('u28','hidden','none',500);
+
+}
+});
+gv_vAlignTable['u38'] = 'top';document.getElementById('u39_img').tabIndex = 0;
+HookHover('u39', false);
+HookClick('u39', false);
+
+u39.style.cursor = 'pointer';
+$axure.eventManager.click('u39', function(e) {
+
+if ((GetWidgetText('u18')) == ('0')) {
+
+SetGlobalVariableValue('CanWeShipVar', 'Yes');
+
+    self.location.href="resources/reload.html#" + encodeURI($axure.globalVariableProvider.getLinkUrl($axure.pageData.url));
+
+}
+
+if ((GetWidgetText('u18')) != ('0')) {
+
+SetGlobalVariableValue('CanWeShipVar', 'Yes');
+
+SetGlobalVariableValue('HasItem', 'Yes');
+
+SetGlobalVariableValue('ItemsInCart', '' + (GetNum(GetGlobalVariableValue('ItemsInCart')) + GetNum(GetWidgetText('u18'))) + '');
+
+    self.location.href="resources/reload.html#" + encodeURI($axure.globalVariableProvider.getLinkUrl($axure.pageData.url));
+
+}
+});
+gv_vAlignTable['u95'] = 'center';gv_vAlignTable['u97'] = 'center';gv_vAlignTable['u99'] = 'center';gv_vAlignTable['u116'] = 'center';gv_vAlignTable['u117'] = 'top';u118.tabIndex = 0;
+
+u118.style.cursor = 'pointer';
+$axure.eventManager.click('u118', function(e) {
+
+if (true) {
+
+	SetPanelVisibility('u108','hidden','none',500);
+
+}
+});
+gv_vAlignTable['u118'] = 'top';document.getElementById('u119_img').tabIndex = 0;
+HookHover('u119', false);
+HookClick('u119', false);
+
+u119.style.cursor = 'pointer';
+$axure.eventManager.click('u119', u119Click);
+InsertAfterBegin(document.body, "<div class='intcases' id='u119LinksClick'></div>")
+var u119LinksClick = document.getElementById('u119LinksClick');
+function u119Click(e) 
+{
+windowEvent = e;
+
+
+	ToggleLinks(e, 'u119LinksClick');
+}
+
+InsertBeforeEnd(u119LinksClick, "<div class='intcaselink' onmouseout='SuppressBubble(event)' onclick='u119Clicku2809cf3445f0485e895187e3184b01e8(event)'>CanShip</div>");
+function u119Clicku2809cf3445f0485e895187e3184b01e8(e)
+{
+
+SetGlobalVariableValue('CanWeShipVar', 'Yes');
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('View_Cart_upsell.html');
+
+	ToggleLinks(e, 'u119LinksClick');
+}
+
+InsertBeforeEnd(u119LinksClick, "<div class='intcaselink' onmouseout='SuppressBubble(event)' onclick='u119Clickuec00e7b9ce4942579db0325946a2fc67(event)'>CantShip</div>");
+function u119Clickuec00e7b9ce4942579db0325946a2fc67(e)
+{
+
+SetGlobalVariableValue('CanWeShipVar', 'No');
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('View_Cart_upsell.html');
+
+	ToggleLinks(e, 'u119LinksClick');
+}
+gv_vAlignTable['u40'] = 'center';gv_vAlignTable['u41'] = 'top';gv_vAlignTable['u42'] = 'top';document.getElementById('u43_img').tabIndex = 0;
+
+u43.style.cursor = 'pointer';
+$axure.eventManager.click('u43', function(e) {
+
+if (true) {
+
+SetWidgetFormText('u47', '' + (GetNum(GetWidgetText('u47')) + 1) + '');
+
+}
+});
+gv_vAlignTable['u44'] = 'top';document.getElementById('u45_img').tabIndex = 0;
+
+u45.style.cursor = 'pointer';
+$axure.eventManager.click('u45', function(e) {
+
+if ((GetWidgetText('u47')) > Number('0')) {
+
+SetWidgetFormText('u47', '' + (GetWidgetText('u47') - 1) + '');
+
+}
+});
+gv_vAlignTable['u46'] = 'top';gv_vAlignTable['u49'] = 'center';gv_vAlignTable['u120'] = 'center';
+$axure.eventManager.focus('u121', function(e) {
+
+if ((GetWidgetText('u121')) == ('Enter Your ZIP Code...')) {
+
+SetWidgetFormText('u121', '');
+
+}
+});
+
+$axure.eventManager.blur('u121', function(e) {
+
+if ((GetWidgetText('u121')) == ('')) {
+
+SetWidgetFormText('u121', 'Enter Your ZIP Code...');
+
+}
+});
+gv_vAlignTable['u122'] = 'top';
+$axure.eventManager.change('u123', function(e) {
+
+if (((GetSelectedOption('u123')) == ('dd')) || (((GetSelectedOption('u124')) == ('mm')) || ((GetSelectedOption('u125')) == ('yyyy')))) {
+
+}
+else
+if (((GetSelectedOption('u123')) != ('dd')) && (((GetSelectedOption('u124')) != ('mm')) && ((GetSelectedOption('u125')) != ('yyyy')))) {
+
+}
+});
+
+$axure.eventManager.focus('u123', function(e) {
+
+if (true) {
+
+}
+});
+
+$axure.eventManager.blur('u123', function(e) {
+
+if (((GetSelectedOption('u123')) == ('dd')) || (((GetSelectedOption('u124')) == ('mm')) || ((GetSelectedOption('u125')) == ('yyyy')))) {
+
+}
+else
+if (((GetSelectedOption('u123')) != ('dd')) && (((GetSelectedOption('u124')) != ('mm')) && ((GetSelectedOption('u125')) != ('yyyy')))) {
+
+}
+});
+
+$axure.eventManager.change('u124', function(e) {
+
+if (((GetSelectedOption('u123')) == ('dd')) || (((GetSelectedOption('u124')) == ('mm')) || ((GetSelectedOption('u125')) == ('yyyy')))) {
+
+}
+else
+if (((GetSelectedOption('u123')) != ('dd')) && (((GetSelectedOption('u124')) != ('mm')) && ((GetSelectedOption('u125')) != ('yyyy')))) {
+
+}
+});
+
+$axure.eventManager.focus('u124', function(e) {
+
+if (true) {
+
+}
+});
+
+$axure.eventManager.blur('u124', function(e) {
+
+if (((GetSelectedOption('u123')) == ('dd')) || (((GetSelectedOption('u124')) == ('mm')) || ((GetSelectedOption('u125')) == ('yyyy')))) {
+
+}
+else
+if (((GetSelectedOption('u123')) != ('dd')) && (((GetSelectedOption('u124')) != ('mm')) && ((GetSelectedOption('u125')) != ('yyyy')))) {
+
+}
+});
+
+$axure.eventManager.change('u125', function(e) {
+
+if (((GetSelectedOption('u123')) == ('dd')) || (((GetSelectedOption('u124')) == ('mm')) || ((GetSelectedOption('u125')) == ('yyyy')))) {
+
+}
+else
+if (((GetSelectedOption('u123')) != ('dd')) && (((GetSelectedOption('u124')) != ('mm')) && ((GetSelectedOption('u125')) != ('yyyy')))) {
+
+}
+else
+if ((GetSelectedOption('u125')) > Number('2007')) {
+
+}
+else
+if ((GetSelectedOption('u125')) > Number('1997')) {
+
+}
+else
+if ((GetSelectedOption('u125')) > Number('1991')) {
+
+}
+else
+if ((GetSelectedOption('u125')) < Number('1991')) {
+
+}
+});
+
+$axure.eventManager.focus('u125', function(e) {
+
+if (true) {
+
+}
+});
+
+$axure.eventManager.blur('u125', function(e) {
+
+if (((GetSelectedOption('u123')) == ('dd')) || (((GetSelectedOption('u124')) == ('mm')) || ((GetSelectedOption('u125')) == ('yyyy')))) {
+
+}
+else
+if (((GetSelectedOption('u123')) != ('dd')) && (((GetSelectedOption('u124')) != ('mm')) && ((GetSelectedOption('u125')) != ('yyyy')))) {
+
+}
+});
+
+$axure.eventManager.focus('u126', function(e) {
+
+if ((GetWidgetText('u126')) == ('Enter Your ZIP Code...')) {
+
+SetWidgetFormText('u126', '');
+
+}
+});
+
+$axure.eventManager.blur('u126', function(e) {
+
+if ((GetWidgetText('u126')) == ('')) {
+
+SetWidgetFormText('u126', 'Enter Your ZIP Code...');
+
+}
+});
+gv_vAlignTable['u2'] = 'center';gv_vAlignTable['u4'] = 'center';gv_vAlignTable['u6'] = 'center';gv_vAlignTable['u8'] = 'center';gv_vAlignTable['u9'] = 'top';gv_vAlignTable['u51'] = 'center';gv_vAlignTable['u53'] = 'center';gv_vAlignTable['u55'] = 'center';gv_vAlignTable['u56'] = 'top';u57.tabIndex = 0;
+
+u57.style.cursor = 'pointer';
+$axure.eventManager.click('u57', function(e) {
+
+if (true) {
+
+	SetPanelState('u28', 'pd0u28','none','',500,'none','',500);
+
+	SetPanelVisibility('u28','hidden','none',500);
+
+}
+});
+gv_vAlignTable['u57'] = 'top';document.getElementById('u58_img').tabIndex = 0;
+HookHover('u58', false);
+HookClick('u58', false);
+
+u58.style.cursor = 'pointer';
+$axure.eventManager.click('u58', function(e) {
+
+if (true) {
+
+SetGlobalVariableValue('CanWeShipVar', 'Yes');
+
+	SetPanelState('u28', 'pd0u28','none','',500,'none','',500);
+
+	SetPanelVisibility('u28','hidden','none',500);
+
+    self.location.href="resources/reload.html#" + encodeURI($axure.globalVariableProvider.getLinkUrl($axure.pageData.url));
+
+}
+});
+gv_vAlignTable['u59'] = 'center';gv_vAlignTable['u130'] = 'center';document.getElementById('u131_img').tabIndex = 0;
+HookHover('u131', false);
+HookClick('u131', false);
+
+u131.style.cursor = 'pointer';
+$axure.eventManager.click('u131', u131Click);
+InsertAfterBegin(document.body, "<div class='intcases' id='u131LinksClick'></div>")
+var u131LinksClick = document.getElementById('u131LinksClick');
+function u131Click(e) 
+{
+windowEvent = e;
+
+
+	ToggleLinks(e, 'u131LinksClick');
+}
+
+InsertBeforeEnd(u131LinksClick, "<div class='intcaselink' onmouseout='SuppressBubble(event)' onclick='u131Clicku9c6b782cba5041cc80ab3ee963034c7a(event)'>Normal</div>");
+function u131Clicku9c6b782cba5041cc80ab3ee963034c7a(e)
+{
+
+SetGlobalVariableValue('ZIPVar', '94025');
+
+SetGlobalVariableValue('Hurry', 'Yes');
+
+	SetPanelVisibility('u0','','none',500);
+
+	BringToFront("u0");
+
+	ToggleLinks(e, 'u131LinksClick');
+}
+
+InsertBeforeEnd(u131LinksClick, "<div class='intcaselink' onmouseout='SuppressBubble(event)' onclick='u131Clicku1f468f46f494479e96e0691cf3d2b79b(event)'>Need Zip</div>");
+function u131Clicku1f468f46f494479e96e0691cf3d2b79b(e)
+{
+
+SetGlobalVariableValue('ZIPVar', 'Enter Your ZIP Code...');
+
+	SetPanelVisibility('u28','','none',500);
+
+	BringToFront("u28");
+
+	ToggleLinks(e, 'u131LinksClick');
+}
+gv_vAlignTable['u132'] = 'center';gv_vAlignTable['u135'] = 'center';gv_vAlignTable['u137'] = 'center';gv_vAlignTable['u139'] = 'top';gv_vAlignTable['u60'] = 'top';gv_vAlignTable['u61'] = 'top';gv_vAlignTable['u63'] = 'center';gv_vAlignTable['u66'] = 'center';gv_vAlignTable['u68'] = 'center';document.getElementById('u69_img').tabIndex = 0;
+HookHover('u69', false);
+HookClick('u69', false);
+
+u69.style.cursor = 'pointer';
+$axure.eventManager.click('u69', function(e) {
+
+if (((GetGlobalVariableValue('Hurry')) == ('Yes')) && ((GetWidgetText('u92')) == ('94025'))) {
+
+SetGlobalVariableValue('CanWeShipVar', 'Yes');
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('View_Cart.html');
+
+}
+
+if ((GetWidgetText('u92')) == ('94025')) {
+
+SetGlobalVariableValue('CanWeShipVar', 'Yes');
+
+	SetPanelState('u28', 'pd2u28','none','',500,'none','',500);
+
+}
+else
+if ((GetWidgetText('u92')) != ('94025')) {
+
+SetGlobalVariableValue('CanWeShipVar', 'No');
+
+	SetPanelState('u28', 'pd1u28','none','',500,'none','',500);
+
+}
+});
+gv_vAlignTable['u141'] = 'top';gv_vAlignTable['u70'] = 'center';gv_vAlignTable['u72'] = 'center';gv_vAlignTable['u74'] = 'center';gv_vAlignTable['u77'] = 'center';gv_vAlignTable['u78'] = 'top';u79.tabIndex = 0;
+
+u79.style.cursor = 'pointer';
+$axure.eventManager.click('u79', function(e) {
+
+if (true) {
+
+	SetPanelState('u64', 'pd1u64','fade','',500,'swing','down',500);
+
+	SetPanelState('u75', 'pd1u75','fade','',500,'fade','',500);
+
+}
+});
+gv_vAlignTable['u79'] = 'top';
