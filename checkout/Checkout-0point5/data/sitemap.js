4d0a2359106508a59db0846fcf8242998f9e19d0
Jack Reis <jack@TastingRoom.com>
2012-09-12 14:47:35 -0700

    updated style sheet requests for first 2 stylesheets to include information about type (text) and media (screen). also removed obsolete code for Collin

--- checkout/Checkout-0point5/data/sitemap.js
+++
@@ -1,8 +0,0 @@
-﻿var sitemap = 
-
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,[_(c,d,e,f,g,h),_(c,i,e,f,g,j,k,[_(c,l,e,f,g,m),_(c,n,e,f,g,o),_(c,p,e,f,g,q),_(c,r,e,f,g,s)])]);}; 
-var b="rootNodes",c="pageName",d="ChangeLog",e="type",f="Wireframe",g="url",h="ChangeLog.html",i="Home",j="Home.html",k="children",l="View Cart",m="View_Cart.html",n="Account",o="Account.html",p="View Cart upsell",q="View_Cart_upsell.html",r="Billing & Shipping",s="Billing___Shipping.html";
-return _creator();
-})();
